7c8025436d2d94224a91be80c1cad298403f5acf
cotrin8672 <gummy8672@gmail.com>
2026-08-24 10:49:46 +0900

    Remove generated test artifacts

--- tools/rotary-decoder-tests/target/x86_64-pc-windows-msvc/debug/deps/rotary_decoder_tests-8b7171c23dfa8cbe.d
+++
@@ -1,8 +0,0 @@
-C:\Users\minol\ghq\github.com\cotrin8672\rmk-config-ReeL\tools\rotary-decoder-tests\target\x86_64-pc-windows-msvc\debug\deps\rotary_decoder_tests-8b7171c23dfa8cbe.d: src\lib.rs src\..\..\..\src\rotary_decoder.rs
-
-C:\Users\minol\ghq\github.com\cotrin8672\rmk-config-ReeL\tools\rotary-decoder-tests\target\x86_64-pc-windows-msvc\debug\deps\librotary_decoder_tests-8b7171c23dfa8cbe.rlib: src\lib.rs src\..\..\..\src\rotary_decoder.rs
-
-C:\Users\minol\ghq\github.com\cotrin8672\rmk-config-ReeL\tools\rotary-decoder-tests\target\x86_64-pc-windows-msvc\debug\deps\librotary_decoder_tests-8b7171c23dfa8cbe.rmeta: src\lib.rs src\..\..\..\src\rotary_decoder.rs
-
-src\lib.rs:
-src\..\..\..\src\rotary_decoder.rs:
--- tools/rotary-decoder-tests/target/x86_64-pc-windows-msvc/debug/deps/rotary_decoder_tests-fb552a18e97cfcaf.d
+++
@@ -1,6 +0,0 @@
-C:\Users\minol\ghq\github.com\cotrin8672\rmk-config-ReeL\tools\rotary-decoder-tests\target\x86_64-pc-windows-msvc\debug\deps\rotary_decoder_tests-fb552a18e97cfcaf.d: src\lib.rs src\..\..\..\src\rotary_decoder.rs
-
-C:\Users\minol\ghq\github.com\cotrin8672\rmk-config-ReeL\tools\rotary-decoder-tests\target\x86_64-pc-windows-msvc\debug\deps\rotary_decoder_tests-fb552a18e97cfcaf.exe: src\lib.rs src\..\..\..\src\rotary_decoder.rs
-
-src\lib.rs:
-src\..\..\..\src\rotary_decoder.rs:
